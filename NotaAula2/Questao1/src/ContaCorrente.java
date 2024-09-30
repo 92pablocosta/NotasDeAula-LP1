bee76fe6aa59f58e54df5c4083192b776bf56d8a
Pablo Costa <164939584+92pablocosta@users.noreply.github.com>
2024-09-30 20:46:08 -0300

    NotaAula2/Questão 1 em andamento

---
+++ NotaAula2/Questao1/src/ContaCorrente.java
@@ -0,0 +1,94 @@
+import java.util.Scanner;
+
+public class ContaCorrente {
+
+    Scanner sc = new Scanner(System.in);
+    private double valor_em_conta;
+    private double valor_deposito;
+    private double valor_saque;
+
+    public ContaCorrente() {
+
+        this.valor_em_conta = 2.57;
+        this.valor_deposito = 0;
+        this.valor_saque = 0;
+
+    }
+
+    private void setValor_em_conta(double valor_em_conta) {
+        this.valor_em_conta = valor_em_conta;
+    }
+
+    private double getValor_em_conta(double valor_deposito) {
+        return valor_em_conta;
+    }
+
+    private void deposito() {
+
+        System.out.print("Quanto gostaria de depositar? R$");
+        valor_deposito = sc.nextDouble();
+        valor_em_conta += valor_deposito;
+        System.out.println("Depósito realizado com sucesso!");
+        System.out.println("Novo Saldo (CC): R$ " + valor_em_conta);
+
+    }
+
+    private void saque() {
+
+        System.out.print("Quanto gostaria de sacar? R$");
+        valor_saque = sc.nextDouble();
+        if (valor_saque > valor_em_conta) {
+            System.out.println("Saldo insuficiente para saque.");
+            System.out.println("Saldo disponível: R$" + valor_em_conta);
+
+            System.out.println("Gostaria de usar o Cheque Especial? [1] - Sim / [2] - Não");
+            int opcao = sc.nextInt();
+            switch (opcao) {
+                case 1:
+                    cheque_especial();
+                    break;
+                case 2:
+                    break;
+            }
+        } else {
+            valor_em_conta -= valor_saque;
+            System.out.println("Saque realizado com sucesso!");
+            System.out.println("Novo Saldo (CC): R$ " + valor_em_conta);
+        }
+
+    }
+
+    private void cheque_especial() {
+        System.out.println("ATENÇÃO! Ao utilizar essa opção sua conta ficará no negativo. ");
+        System.out.println("Limite disponível no Cheque Especial de R$ 1.000,00");
+        System.out.println("Gostaria de continuar? [1] - Sim / [2] - Não");
+        int opcao = sc.nextInt();
+        switch (opcao) {
+            case 1:
+                System.out.println("Quanto gostaria de sacar? R$");
+                valor_saque = sc.nextDouble();
+
+
+                if ((valor_em_conta - valor_saque) < -1000) {
+
+                    System.out.println("Valor solicitado excede o limite do Cheque. Tente novamente.");
+                    break;
+
+                } else {
+
+                    valor_em_conta -= valor_saque;
+                    System.out.println("Saque realizado com sucesso!");
+                    System.out.println("Novo Saldo (CC): R$ " + valor_em_conta);
+                    break;
+
+                }
+
+            case 2:
+                break;
+        }
+
+    }
+
+
+
+}
--- NotaAula2/Questao1/src/Main.java
+++ NotaAula2/Questao1/src/Main.java
@@ -1,7 +1,45 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        
+        Scanner sc = new Scanner(System.in);
+        // Variaveis
+        int opcao;
+        double conta_corrente;
+        double poupanca;
+        String correntista;
+        String numero_conta;
+        int numero_agencia;
+
+        System.out.println("-----Bem vindo ao aplicativo do Banco Paradise-----");
+
+        System.out.println("Insira o nome do titular da conta: ");
+        correntista = sc.nextLine();
+
+        System.out.println("Insira o numero da sua conta: ");
+        numero_conta = sc.nextLine();
+
+        System.out.println("Insira o numero de agencia: ");
+        numero_agencia = sc.nextInt();
+
+        ContaCorrente cc = new ContaCorrente();
+        ContaPoupanca pp = new ContaPoupanca();
+
+        do {
+            System.out.println("Bem vindo, " + correntista + "! Selecione uma opção abaixo:");
+            System.out.println("1 - Acessar conta corrente");
+            System.out.println("2 - Acessar poupanca");
+            System.out.println("3 - Sair");
+            opcao = sc.nextInt();
+
+            switch (opcao) {
+                case 1:
+
+
+            }
+
+        } while (opcao != 3);
 
     }
 }
